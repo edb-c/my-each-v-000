a30843f20c48520d0af6b1b3ad1bfcf14be984bd
Ethel C <cofresi.ethel@gmail.com>
2018-05-17 01:19:23 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,8 @@
-def my_each(collection) do |i|
-  puts i
+def my_each(arr)
+  i = 0
+  while i < arr.length
+    yield(arr[i])
+    i += 1
+  end
+  arr
 end
-end
-collection = [1, 2, 3, 4]
-my_each(collection)
